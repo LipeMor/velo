6091142c76de7575a40faa77d705f85d8d77beef
Filipe Moreira <correio.filipe@gmail.com>
2026-07-29 11:13:00 -0300

    Ultimas alterações do projeto

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,16 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  //timeout: 5000,
+
+  // Tempo máximo para cada teste completo (30 segundos é o padrão)
+  timeout: 60_000,
+
+  // Tempo máximo para assertions (toBeVisible(), toContainText(), etc.) 5 segundos é o padrão
+  expect: {
+    timeout: 5_000, //não vale a pena aumentar, pois o teste  pode ficar lento no tempo de execução, vale a pena usar o timeout explicito.
+  },
+
+
   testDir: './playwright/e2e',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -30,7 +39,15 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
+
+    // Tempo máximo para ações interativas (clicar, preencher, etc.) 
+    // Quando o valor é 0 herda o limite do timeout geral do teste.
+    actionTimeout: 5_000, 
+
+    // Tempo máximo para navegações como goto(), waitForURL(), waitForLoadState(), etc. 
+    // Quando o valor é 0 também herda o limite do timeout geral do teste.
+    navigationTimeout: 10_000, 
   },
 
   /* Configure projects for major browsers */
--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,27 +1,212 @@
 import { test, expect } from '@playwright/test';
+import { generateOrderCode } from '../support/helpers';
+import { OrderLookupPage } from '../support/pages/OrderLookupPage';
 
 // AAA - Arrange, Act, Assert
 
-test('deve consultar um pedido aprovado', async ({ page }) => {
-  // Arrange
-  await page.goto('http://localhost:5173/');
-  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
-  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-  await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
+test.describe('Consulta de Pedido', () => {
 
-  //Act
-  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-T7QNY0');
-  await page.getByRole('button', { name: 'Buscar Pedido' }).click();
+  test.beforeEach(async ({ page }) => {
+    // Arrange
+    await page.goto('http://localhost:5173/');
+    await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
 
-  // Assert
-  
-  await expect(page.getByText('VLO-T7QNY0')).toBeVisible({ timeout: 10_000 });
-  await expect(page.getByText('APROVADO')).toBeVisible();
+    await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+    await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
+  })
 
-  // await expect(page.getByTestId('order-result-id')).toBeVisible({ timeout: 10_000 });
-  // await expect(page.getByTestId('order-result-id')).toContainText('VLO-T7QNY0');
-  
+  test('deve consultar um pedido aprovado', async ({ page }) => {
+    // Test Data
 
-  // await page.getByText('VLO-T7QNY0').toBeVisible();
-  // await page.getByText('APROVADO').toBeVisible();
-});
+    const order = {
+      number: 'VLO-QNE15F',
+      status: 'APROVADO',
+      color: 'Midnight Black',
+      wheels: 'sport Wheels',
+      customer: {
+        name: 'FILIPE SANTOS',
+        email: 'filipeqa@gmail.com',
+      },
+      payment: 'À Vista',
+    }
+
+    //Act
+    const orderLookupPage = new OrderLookupPage(page);    
+    await orderLookupPage.searchOrder(order.number);
+
+    // Assert
+
+    await expect(page.getByTestId(`order-result-${order.number}`)).toMatchAriaSnapshot(`
+      - img
+      - paragraph: Pedido
+      - paragraph: ${order.number}
+      - status:
+        - img
+        - text: ${order.status}
+      - img "Velô Sprint"
+      - paragraph: Modelo
+      - paragraph: Velô Sprint
+      - paragraph: Cor
+      - paragraph: ${order.color}
+      - paragraph: Interior
+      - paragraph: cream
+      - paragraph: Rodas
+      - paragraph: ${order.wheels}
+      - heading "Dados do Cliente" [level=4]
+      - paragraph: Nome
+      - paragraph: ${order.customer.name}
+      - paragraph: Email
+      - paragraph: ${order.customer.email}
+      - paragraph: Loja de Retirada
+      - paragraph
+      - paragraph: Data do Pedido
+      - paragraph: /\\d+\\/\\d+\\/\\d+/
+      - heading "Pagamento" [level=4]
+      - paragraph: ${order.payment}
+      - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+      `);
+
+    const statusBadge = page.getByRole('status').filter({ hasText: order.status });
+
+    await expect(statusBadge).toHaveClass(/bg-green-100/);
+    await expect(statusBadge).toHaveClass(/text-green-700/);
+
+    const statusIcon = statusBadge.locator('svg');
+    await expect(statusIcon).toHaveClass(/lucide-circle-check-big/);
+
+  });
+
+  test('deve consultar um pedido reprovado', async ({ page }) => {
+    // Test Data
+
+    const order = {
+      number: 'VLO-8XRTQ1',
+      status: 'REPROVADO',
+      color: 'Midnight Black',
+      wheels: 'sport Wheels',
+      customer: {
+        name: 'Steve Jobs',
+        email: 'jobs@apple.com',
+      },
+      payment: 'À Vista',
+    }
+
+    //Act
+    const orderLookupPage = new OrderLookupPage(page);    
+    await orderLookupPage.searchOrder(order.number);
+
+    await expect(page.getByTestId(`order-result-${order.number}`)).toMatchAriaSnapshot(`
+      - img
+      - paragraph: Pedido
+      - paragraph: ${order.number}
+      - status:
+        - img
+        - text: ${order.status}
+      - img "Velô Sprint"
+      - paragraph: Modelo
+      - paragraph: Velô Sprint
+      - paragraph: Cor
+      - paragraph: ${order.color}
+      - paragraph: Interior
+      - paragraph: cream
+      - paragraph: Rodas
+      - paragraph: ${order.wheels}
+      - heading "Dados do Cliente" [level=4]
+      - paragraph: Nome
+      - paragraph: ${order.customer.name}
+      - paragraph: Email
+      - paragraph: ${order.customer.email}
+      - paragraph: Loja de Retirada
+      - paragraph 
+      - paragraph: Data do Pedido
+      - paragraph: /\\d+\\/\\d+\\/\\d+/
+      - heading "Pagamento" [level=4]
+      - paragraph: ${order.payment}
+      - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+      `);
+
+    const statusBadge = page.getByRole('status').filter({ hasText: order.status });
+
+    await expect(statusBadge).toHaveClass(/bg-red-100/);
+    await expect(statusBadge).toHaveClass(/text-red-700/);
+
+    const statusIcon = statusBadge.locator('svg');
+    await expect(statusIcon).toHaveClass(/lucide-circle-x/);
+  });
+
+  test('deve consultar um pedido em análise', async ({ page }) => {
+    // Test Data
+
+    const order = {
+      number: 'VLO-P13BFS',
+      status: 'EM_ANALISE',
+      color: 'Glacier Blue',
+      wheels: 'aero Wheels',
+      customer: {
+        name: 'João da Silva',
+        email: 'joao@velo.dev',
+      },
+      payment: 'À Vista',
+    }
+    
+    //Act
+    const orderLookupPage = new OrderLookupPage(page);    
+    await orderLookupPage.searchOrder(order.number);
+
+    await expect(page.getByTestId(`order-result-${order.number}`)).toMatchAriaSnapshot(`
+      - img
+      - paragraph: Pedido
+      - paragraph: ${order.number}
+      - status:
+        - img
+        - text: ${order.status}
+      - img "Velô Sprint"
+      - paragraph: Modelo
+      - paragraph: Velô Sprint
+      - paragraph: Cor
+      - paragraph: ${order.color}
+      - paragraph: Interior
+      - paragraph: cream
+      - paragraph: Rodas
+      - paragraph: ${order.wheels}
+      - heading "Dados do Cliente" [level=4]
+      - paragraph: Nome
+      - paragraph: ${order.customer.name}
+      - paragraph: Email
+      - paragraph: ${order.customer.email}
+      - paragraph: Loja de Retirada
+      - paragraph 
+      - paragraph: Data do Pedido
+      - paragraph: /\\d+\\/\\d+\\/\\d+/
+      - heading "Pagamento" [level=4]
+      - paragraph: ${order.payment}
+      - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+      `);
+
+    const statusBadge = page.getByRole('status').filter({ hasText: order.status });
+
+    await expect(statusBadge).toHaveClass(/bg-amber-100/);
+    await expect(statusBadge).toHaveClass(/text-amber-700/);
+
+    const statusIcon = statusBadge.locator('svg');
+    await expect(statusIcon).toHaveClass(/lucide-clock/);
+  });
+
+
+  test('deve exibir mensagem quando o pedido não é encontrado', async ({ page }) => {
+    // Test Data
+    const order = generateOrderCode();
+
+    // Act
+    const orderLookupPage = new OrderLookupPage(page);    
+    await orderLookupPage.searchOrder(order);
+
+    await expect(page.locator('#root')).toMatchAriaSnapshot(`
+    - img
+    - heading "Pedido não encontrado" [level=3]
+    - paragraph: Verifique o número do pedido e tente novamente
+    `);
+
+  })
+})
+
